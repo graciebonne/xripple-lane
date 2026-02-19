5f27189a3857003fc34a964496ee9a85da5791fd
gracie <stanzhenry@gmail.com>
2026-02-19 10:45:20 +0100

    Update Header.tsx

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -47,19 +47,19 @@ const Header = () => {
     }
 }
  
-// Run the function every 500ms to catch the widget when it finishes loading
-// const translateInterval = setInterval(() => {
-//     const select = document.querySelector('.goog-te-combo');
-//     if (select) {
-//         abbreviateLanguages();
-//         // We don't clear the interval because Google sometimes re-renders the element
-//     }
-// }, 000); 
-     const select = document.querySelector('.goog-te-combo');
+//Run the function every 500ms to catch the widget when it finishes loading
+const translateInterval = setInterval(() => {
+    const select = document.querySelector('.goog-te-combo');
     if (select) {
         abbreviateLanguages();
         // We don't clear the interval because Google sometimes re-renders the element
     }
+}, 20000); 
+    //  const select = document.querySelector('.goog-te-combo');
+    // if (select) {
+    //     abbreviateLanguages();
+    //     // We don't clear the interval because Google sometimes re-renders the element
+    // }
 //   window.googleTranslateElementInit = () => {
 //   new window.google.translate.TranslateElement(
 //     { pageLanguage: "en" },
